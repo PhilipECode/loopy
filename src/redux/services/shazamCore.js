3c3a487f4e217f3b3f22517dc3622083e0c99d5c
Philip <philip.eemv@gmail.com>
2022-10-05 02:53:34 +0300

    initial commit /discover, /music player, /file structure, /shazam api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '0ba079ac0emsh662d5f11b9c4d94p113b10jsn6174ac5521bf');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
